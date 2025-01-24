75ae04402b222cf0aa54e3c0c2933ecf8afbea55
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-01-24 10:00:32 +0000

    add books listviews

--- src/main/java/hm/springframework/sprin6webappp/services/BookServiceImpl.java
+++ src/main/java/hm/springframework/sprin6webappp/services/BookServiceImpl.java
@@ -2,7 +2,9 @@ package hm.springframework.sprin6webappp.services;
 
 import hm.springframework.sprin6webappp.domain.Book;
 import hm.springframework.sprin6webappp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService{
 
     private final BookRepository bookRepository;
